6fa24136f1a345aba78608fb34e8d1c2098b3287
ShyamSai,Uppuluri <shyamsai357@gmail.com>
2023-12-19 00:05:08 +0530

    Q1.How many apps which are free and for a certain genre and launched in a particular year etc?

--- src/main/scala/org/segwise/Segwise.scala
+++ src/main/scala/org/segwise/Segwise.scala
@@ -1,6 +1,8 @@
 package org.segwise
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.expressions.Window
+import org.apache.spark.sql.functions.{desc, rank}
 
 object Segwise {
   def main(args: Array[String]): Unit = {
@@ -10,12 +12,98 @@ object Segwise {
       .master("local[*]")
       .getOrCreate()
 
+    import spark.implicits._
+
     val df = spark.read
       .option("header", "true")
       .option("inferSchema", "true")
       .csv("src/main/resources/input/records1000")
+    //.csv("src/main/resources/input/original/google-play-dataset-by-tapivedotcom.csv")
 
     df.show()
     df.printSchema()
+
+    /*
+    df schema is as below:
+    root
+      |-- _c0: string (nullable = true)
+      |-- appId: string (nullable = true)
+      |-- developer: string (nullable = true)
+      |-- developerId: string (nullable = true)
+      |-- developerWebsite: string (nullable = true)
+      |-- free: string (nullable = true)
+      |-- genre: string (nullable = true)
+      |-- genreId: string (nullable = true)
+      |-- inAppProductPrice: string (nullable = true)
+      |-- minInstalls: string (nullable = true)
+      |-- offersIAP: string (nullable = true)
+      |-- originalPrice: string (nullable = true)
+      |-- price: string (nullable = true)
+      |-- ratings: string (nullable = true)
+      |-- len screenshots: string (nullable = true)
+      |-- adSupported: string (nullable = true)
+      |-- containsAds: string (nullable = true)
+      |-- reviews: string (nullable = true)
+      |-- releasedDayYear: string (nullable = true)
+      |-- sale: string (nullable = true)
+      |-- score: string (nullable = true)
+      |-- summary: string (nullable = true)
+      |-- title: string (nullable = true)
+      |-- updated: string (nullable = true)
+      |-- histogram1: string (nullable = true)
+      |-- histogram2: string (nullable = true)
+      |-- histogram3: string (nullable = true)
+      |-- histogram4: string (nullable = true)
+      |-- histogram5: string (nullable = true)
+      |-- releasedDay: string (nullable = true)
+      |-- releasedYear: string (nullable = true)
+      |-- releasedMonth: string (nullable = true)
+      |-- dateUpdated: string (nullable = true)
+      |-- minprice: string (nullable = true)
+      |-- maxprice: string (nullable = true)
+      |-- ParseReleasedDayYear: string (nullable = true)
+     */
+
+
+    //Cast ratings column to integer
+    val df1 = df.withColumn("rating", $"ratings".cast("Int"))
+
+
+    //Q1.How many apps which are free and for a certain genre and launched in a particular year etc?
+
+    // df2 --> Top Free apps for ART_AND_DESIGN genre in 2018 descending order by ratings
+    val df2 = df1.select("title", "releasedYear", "free", "genreId", "ratings").where("free = 1 AND genreId = 'ART_AND_DESIGN' AND releasedYear = 2018").sort(desc("rating"))
+    df2.show(1000, false)
+    /*
+      +-----------------------------------------------------------+------------+----+--------------+-------+
+      |title                                                      |releasedYear|free|genreId       |ratings|
+      +-----------------------------------------------------------+------------+----+--------------+-------+
+      |Thumbnail Maker - Channel art                              |2018        |1   |ART_AND_DESIGN|149626 |
+      |Adobe Express: Graphic Design                              |2018        |1   |ART_AND_DESIGN|126331 |
+      |Text on Photo - Text to Photos                             |2018        |1   |ART_AND_DESIGN|103510 |
+      |Unwanted Object Remover - Remo                             |2018        |1   |ART_AND_DESIGN|102748 |
+      |X Launcher: With OS13 Theme                                |2018        |1   |ART_AND_DESIGN|95020  |
+      |Easy Pose - 3D pose making app                             |2018        |1   |ART_AND_DESIGN|89525  |
+      |Anime Wallpaper HD | 4K List                               |2018        |1   |ART_AND_DESIGN|71595  |
+      |Poster Maker, Flyer Maker                                  |2018        |1   |ART_AND_DESIGN|69814  |
+     */
+
+
+    // df3 --> Top 3 free app in each genreId descending order by ratings
+    val df3 = df1.select("title", "genreId", "rating").where("free = 1").withColumn("rank", rank().over(Window.partitionBy("genreId").orderBy($"rating".desc))).filter($"rank" <= 3).drop("rank")
+    df3.show(1000, false)
+    /*
+    +-----------------------------------------------------------------+-------------------+---------+
+    |title                                                            |genreId            |rating   |
+    +-----------------------------------------------------------------+-------------------+---------+
+    |Ticketmaster?Buy, Sell Tickets                                  |EVENTS             |111327   |
+    |Invitation maker & Card design                                   |EVENTS             |107255   |
+    |StubHub - Live Event Tickets                                     |EVENTS             |75082    |
+    |WEBTOON                                                          |COMICS             |2809227  |
+    |MangaToon - Manga Reader                                         |COMICS             |620418   |
+    |Translator Women's Voice - TTS                                   |COMICS             |460783   |
+    |Cricbuzz - Live Cricket Scores                                   |SPORTS             |1557176  |
+    |OneFootball - Soccer Scores                                      |SPORTS             |1536446  |
+     */
   }
 }
